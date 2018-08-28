7d46d5765f64cbc6a7c12feb0ec998539f95b5d5
Tony Smith <tsmith.sounds@gmail.com>
2018-08-28 18:42:05 -0500

    Working through components module, finished exercise 6.

---
+++ Exercises/Start/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+      <my-header></my-header>
+        <hr>
+      <div class="row">
+        <my-servers></my-servers>
+        <my-server-status></my-server-status>
+      </div>
+        <hr>
+      <my-footer></my-footer>
+    </div>
+</template>
+
+<script>
+  import Header from './Header.vue'
+  import Footer from './Footer.vue'
+  import Servers from './Servers.vue'
+  import ServerStatus from './ServerStatus.vue'
+
+  export default {
+    components: {
+        'my-header': Header,
+        'my-footer': Footer,
+        'my-servers': Servers,
+        'my-server-status': ServerStatus
+    }
+  }
+</script>
+
+<style>
+
+</style>
